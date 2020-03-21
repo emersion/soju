4b9168196a8c5f40011d9dcfc61f27ec04e0c03e
Simon Ser <contact@emersion.fr>
2020-03-21 08:44:03 +0100

    Fix typo

--- downstream.go
+++ downstream.go
@@ -454,7 +454,7 @@ func (dc *downstreamConn) handleMessageUnregistered(msg *irc.Message) error {
 			})
 		} else {
 			challengeStr := "+"
-			if len(challenge) == 0 {
+			if len(challenge) > 0 {
 				challengeStr = base64.StdEncoding.EncodeToString(challenge)
 			}
 
