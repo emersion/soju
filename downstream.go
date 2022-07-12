emersion/soju

commit 7e21e79eaba0a574a14e58b76bf0d8b1f55bb44e

downstream: fix MARKREAD/READ command name in broadcast
We wouldn't send a MARKREAD message to draft/read-marker downstreams
when receiving a READ command.

downstream.go
@@ -2983,9 +2983,13 @@ func (dc *downstreamConn) handleMessageRegistered(ctx context.Context, msg *irc.
 		}
 		network.forEachDownstream(func(d *downstreamConn) {
 			if broadcast || dc.id == d.id {
+				cmd := "MARKREAD"
+				if !dc.caps.IsEnabled("draft/read-marker") {
+					cmd = "READ"
+				}
 				d.SendMessage(&irc.Message{
 					Prefix:  d.prefix(),
-					Command: msg.Command,
+					Command: cmd,
 					Params:  []string{d.marshalEntity(network, entity), timestampStr},
 				})
 			}

commit c69ea81999d334ac7949b6a406555d588e17dae8

downstream: fix downstream check for draft/read-marker
`dc` is the source connection, `d` is the destination for the
broadcast.

Fixes: 7e21e79eaba0 ("downstream: fix MARKREAD/READ command name in broadcast")

downstream.go
@@ -3017,7 +3017,7 @@ func (dc *downstreamConn) handleMessageRegistered(ctx context.Context, msg *irc.
 		network.forEachDownstream(func(d *downstreamConn) {
 			if broadcast || dc.id == d.id {
 				cmd := "MARKREAD"
-				if !dc.caps.IsEnabled("draft/read-marker") {
+				if !d.caps.IsEnabled("draft/read-marker") {
 					cmd = "READ"
 				}
 				d.SendMessage(&irc.Message{