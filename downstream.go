emersion/soju

commit 5e56cc30c538eb88c21cce732e2e357e80098164

downstream: fix inverted range in CHATHISTORY LATEST with a timestamp

downstream.go
@@ -2913,8 +2913,10 @@ func (dc *downstreamConn) handleMessageRegistered(ctx context.Context, msg *irc.
 
 		var history []*irc.Message
 		switch subcommand {
-		case "BEFORE", "LATEST":
+		case "BEFORE":
 			history, err = store.LoadBeforeTime(ctx, bounds[0], time.Time{}, &options)
+		case "LATEST":
+			history, err = store.LoadBeforeTime(ctx, time.Now(), bounds[0], &options)
 		case "AFTER":
 			history, err = store.LoadAfterTime(ctx, bounds[0], time.Now(), &options)
 		case "BETWEEN":

commit bb868eae82c3b4860dfd19a602850f6cfc923212

downstream: fix CHATHISTORY LATEST without a bound
Fixes: 5e56cc30c538 ("downstream: fix inverted range in CHATHISTORY LATEST with a timestamp")

downstream.go
@@ -2878,7 +2878,7 @@ func (dc *downstreamConn) handleMessageRegistered(ctx context.Context, msg *irc.
 		var bounds [2]time.Time
 		bounds[0] = parseChatHistoryBound(boundsStr[0])
 		if subcommand == "LATEST" && boundsStr[0] == "*" {
-			bounds[0] = time.Now()
+			bounds[0] = time.Time{}
 		} else if bounds[0].IsZero() {
 			return ircError{&irc.Message{
 				Command: "FAIL",