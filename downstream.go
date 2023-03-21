emersion/soju

commit 4ca8dfa37d2719f8272354c138de8412b55d4bf0

downstream: fix nil pointer deref in downstreamConn.setUser
References: https://lists.sr.ht/~emersion/soju-dev/%3CCR6PX9SU5FSO.3UR92LHKXOXML%40hades.moritz.sh%3E

downstream.go
@@ -1249,7 +1249,7 @@ func unmarshalUsername(rawUsername string) (username, client, network string) {
 
 func (dc *downstreamConn) setUser(username, clientName, networkName string) error {
 	dc.user = dc.srv.getUser(username)
-	if dc.user == nil && dc.user.srv.Config().EnableUsersOnAuth {
+	if dc.user == nil && dc.srv.Config().EnableUsersOnAuth {
 		ctx := context.TODO()
 		if _, err := dc.user.srv.db.GetUser(ctx, username); err != nil {
 			// Can't find the user in the DB -- try to create it

commit 9579998e630992113c0a9817c8ee9e265919cf61

downstream: fix panic due to nil user in setUser
Fixes: 4ca8dfa37d27 ("downstream: fix nil pointer deref in downstreamConn.setUser")

downstream.go
@@ -1251,13 +1251,13 @@ func (dc *downstreamConn) setUser(username, clientName, networkName string) erro
 	dc.user = dc.srv.getUser(username)
 	if dc.user == nil && dc.srv.Config().EnableUsersOnAuth {
 		ctx := context.TODO()
-		if _, err := dc.user.srv.db.GetUser(ctx, username); err != nil {
+		if _, err := dc.srv.db.GetUser(ctx, username); err != nil {
 			// Can't find the user in the DB -- try to create it
 			record := database.User{
 				Username: username,
 				Enabled:  true,
 			}
-			dc.user, err = dc.user.srv.createUser(ctx, &record)
+			dc.user, err = dc.srv.createUser(ctx, &record)
 			if err != nil {
 				return fmt.Errorf("failed to automatically create user %q after successful authentication: %v", username, err)
 			}