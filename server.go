67292971593ecda868ca1cc17415f48da8a36662
Simon Ser <contact@emersion.fr>
2023-12-09 01:30:48 +0100

    server: fix malformed Web Push subscriber URI
    The library prepends "mailto:".

--- server.go
+++ server.go
@@ -339,10 +339,12 @@ func (s *Server) sendWebPush(ctx context.Context, sub *webpush.Subscription, vap
 		},
 		VAPIDPublicKey:  s.webPush.VAPIDKeys.Public,
 		VAPIDPrivateKey: s.webPush.VAPIDKeys.Private,
-		Subscriber:      "https://soju.im",
-		TTL:             7 * 24 * 60 * 60, // seconds
-		Urgency:         urgency,
-		RecordSize:      2048,
+		// TODO: switch back to an HTTP URL once this is merged:
+		// https://github.com/SherClockHolmes/webpush-go/pull/57
+		Subscriber: "webpush@soju.im",
+		TTL:        7 * 24 * 60 * 60, // seconds
+		Urgency:    urgency,
+		RecordSize: 2048,
 	}
 
 	if vapidPubKey != options.VAPIDPublicKey {
