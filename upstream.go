emersion/soju

commit cef94ff662a61153460cc116044743b836b84ea8

upstream: sleep for a bit after sending connect commands
Workaround for OFTC which still doesn't have SASL…

upstream.go
@@ -2131,6 +2131,18 @@ func (uc *upstreamConn) runUntilRegistered(ctx context.Context) error {
 		}
 	}
 
+	// Connect commands usually send a message to NickServ or similar, which
+	// asynchronously informs the IRC server that the user is logged in. Some
+	// channels require the user to be logged in to be joined. Work around
+	// these old networks by leaving 3 seconds for the bot to react.
+	if len(uc.network.ConnectCommands) > 0 {
+		uc.logger.Debugf("sent %d connect command(s)", len(uc.network.ConnectCommands))
+		select {
+		case <-ctx.Done():
+		case <-time.After(3 * time.Second):
+		}
+	}
+
 	return nil
 }
 

commit 5e89246e6457a8313c837990cbc7d008f554103b

upstream: fix delay after connect commands
The rationale behind these is to add a delay until the NickServ
processes these commands which often authenticate the user to
successfully join channels that require the user to be authenticated
first. As such it must be ensured that the delay happens *before*
attempting to autojoin channels, which wasn't the case. Fix that.

Fixes: cef94ff662a6 ("upstream: sleep for a bit after sending connect commands")

upstream.go
@@ -1018,18 +1018,6 @@ func (uc *upstreamConn) handleMessage(ctx context.Context, msg *irc.Message) err
 		uc.registered = true
 		uc.serverPrefix = msg.Prefix
 		uc.logger.Printf("connection registered with nick %q", uc.nick)
-
-		if uc.network.channels.Len() > 0 {
-			var channels, keys []string
-			uc.network.channels.ForEach(func(_ string, ch *database.Channel) {
-				channels = append(channels, ch.Name)
-				keys = append(keys, ch.Key)
-			})
-
-			for _, msg := range xirc.GenerateJoin(channels, keys) {
-				uc.SendMessage(ctx, msg)
-			}
-		}
 	case irc.RPL_MYINFO:
 		if err := parseMessageParams(msg, nil, &uc.serverName, nil, &uc.availableUserModes, nil); err != nil {
 			return err

user.go
@@ -254,6 +254,18 @@ func (net *network) runConn(ctx context.Context) error {
 		return fmt.Errorf("failed to register: %w", err)
 	}
 
+	if uc.network.channels.Len() > 0 {
+		var channels, keys []string
+		uc.network.channels.ForEach(func(_ string, ch *database.Channel) {
+			channels = append(channels, ch.Name)
+			keys = append(keys, ch.Key)
+		})
+
+		for _, msg := range xirc.GenerateJoin(channels, keys) {
+			uc.SendMessage(ctx, msg)
+		}
+	}
+
 	net.user.events <- eventUpstreamConnected{uc}
 	defer func() {
 		net.user.events <- eventUpstreamDisconnected{uc}